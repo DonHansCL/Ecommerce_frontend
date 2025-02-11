f98de679890a0ae3bcfdfedd3311b3cb9c63ca3f
DonHansCL <hansenglishteacher@gmail.com>
2025-02-11 08:25:38 -0300

    arreglo boton explorar banner

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,7 @@
 // src/components/Banner.js
 import React from 'react';
 import 'react-responsive-carousel/lib/styles/carousel.min.css';
-import Link from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 function Banner() {
   return (
